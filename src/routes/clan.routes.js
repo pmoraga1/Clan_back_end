91599b2bfaff80669199764c1b6fb929383d2f16
pmoraga1 <paulimoraga@gmail.com>
2024-02-22 16:17:37 -0300

    [FEAT] - Clan controllers: edit-deleteCredentials, getClan

--- src/routes/clan.routes.js
+++ src/routes/clan.routes.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const clanRouter = express.Router()
-const {createClan, editClan, deleteClan, getClan, addMember, deleteMember, editCredentials, deleteCredentials} = require("../controllers/clan.controllers.js")
+const {createClan, deleteClan, getClan, addMember, deleteMember, editCredentials, deleteCredentials} = require("../controllers/clan.controllers.js")
 
 clanRouter.post("/create", createClan)
-
-clanRouter.patch("/edit", editClan)
 
 clanRouter.patch("/addmember", addMember)
 
